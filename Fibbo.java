86a5a7deb51632ee542b048495bb80541c7a8989
ben trettin <ben trettin>
2016-12-04 20:49:08 -0600

    I added comments and changed number of iterations

--- Fibbo.java
+++ Fibbo.java
@@ -1,11 +1,15 @@
 
 public class Fibbo {
 
+	/**
+	 * 
+	 * This gives some wierd numbers bro
+	 */
 	public static void main(String[] args) {
 		double first=1;
 		double second=1;
 		double third=2;
-		for(int a=0; a<100; a=a+1){
+		for(int a=0; a<110; a=a+1){
 			double next=second+third;
 			System.out.println(first);
 			first=second;
